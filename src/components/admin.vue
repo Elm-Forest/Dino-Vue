38bfbb437f1c40e481cb442e31176600562cd3be
Elm Forest <timbrightland@gmail.com>
2023-02-01 21:27:36 +0800

    更新管理员导航栏

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -1,27 +1,31 @@
 <template>
   <el-container class="home-container">
     <!-- 头部 -->
-    <el-header style="height:90px">
-      <div>
-        <span id="title">OA自动化办公系统</span>
+    <el-header>
+      <div class="head_img">
+        <el-avatar :src="headImg.dept" class="el-dropdown-link" id="headImg" alt="" :size="30"
+                   @mouseover="showInfo" @mouseleave="hideInfo"></el-avatar>
       </div>
-      <el-dropdown>
-        <img src="../images/background.jpg" class="el-dropdown-link" @mouseover="showInfo" @mouseleave="hideInfo" alt=""
-             id="headImg">
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item @click.native="userinfo">查看账户详细信息</el-dropdown-item>
-          <el-dropdown-item
-              @click.native="logout">退出
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
+      <span id="title" style="position: absolute;display: block;margin-left: 20px;">{{ name.dept }} | {{ role }}</span>
+      <span id="title" style="position: absolute;display: block;right: 60px">欢迎您，{{ name.user }}</span>
+      <div class="head_img">
+        <el-dropdown>
+          <el-avatar :src="headImg.user" class="el-dropdown-link" id="headImg" alt="" :size="30"
+                     @mouseover="showInfo" @mouseleave="hideInfo"></el-avatar>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item @click.native="userinfo">账户详情</el-dropdown-item>
+            <el-dropdown-item @click.native="logout">退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+
     </el-header>
     <!-- 主体 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">
+      <el-aside width="250px">
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu background-color="#fafafa">
           <!-- 1.文档管理平台 -->
           <el-submenu index="1">
             <template slot="title">
@@ -126,45 +130,90 @@
 </template>
 
 <script>
+import user_img from '../images/background.jpg'
+import dept_img from '../images/corp_default.png'
+import '../utils/role.js'
+
+
 export default {
   data() {
     return {
-      userInfo: false
+      headImg: {
+        user: user_img,
+        dept: dept_img
+      },
+      userInfo: false,
+      name: {
+        user: '路人甲',
+        dept: 'OA办公自动化'
+      },
+      role: '未指定部门'
     }
   },
   created() {
-    this.getUserHeadImg()
+    this.getUserHeadImg();
+    this.getBaseInfo();
   },
+  mounted: {},
   methods: {
     userinfo() {
-      console.log(666)
-      this.$router.push('/admin/userinfo');
+      this.$router.push('/userinfo');
     },
     getUserHeadImg() {
+      const this_vue = this;
       this.$axios({
         method: 'get',
         url: '/user/img',
       }).then(function (response) {
-        document.getElementById('headImg').src = response.data;
+        this_vue.headImg.user = response.data;
         console.log(JSON.stringify(response.data));
       }).catch(function (error) {
         console.log(error);
       });
     },
+    getBaseInfo() {
+      const this_vue = this;
+      this.$axios({
+        method: 'GET',
+        url: '/user/userinfo/base',
+      }).then(res => {
+        let userName = res.data.userName;
+        let deptName = res.data.deptName;
+        let role = res.data.role;
+        let deptImg = res.data.deptUrl;
+        if (userName === null || userName === '') {
+          userName = '普通用户(未命名)';
+        }
+        this_vue.name.user = userName;
+        if (deptName === null || deptName === '') {
+          deptName = '未命名企业'
+        }
+        this_vue.name.dept = deptName;
+        if (deptImg === null || deptImg === '') {
+          deptImg = dept_img
+        }
+        this_vue.headImg.dept = deptImg;
+        if (role === null || role === '') {
+          role = '未指定部门'
+        } else {
+          role = roles[role - 1]
+        }
+        console.log(role)
+        this_vue.role = role;
+      })
+    },
     hideInfo() {
       setTimeout(() => {
         this.userInfo = false
       }, 3000)
-      console.log(1)
     },
-//当触发mouseover时调用的方法       
+//当触发mouseover时调用的方法
     showInfo() {
       this.userInfo = true
-      console.log(1)
     },
     // 返回登录界面
     logout() {
-      var this_vue = this
+      let this_vue = this
       this.$axios({
         method: 'post',
         url: '/user/logout',
@@ -175,11 +224,9 @@ export default {
           message: '您已登出！',
           type: 'warning'
         })
-        console.log(JSON.stringify(response.data));
       }).catch(function (error) {
         console.log(error);
-      });
-
+      })
     },
     // 查看消息按钮
     showNews() {
@@ -250,28 +297,56 @@ export default {
 </script>
 
 <style scoped>
+.el-aside {
+  border-right: 1px solid #e8e8e8;
+  box-shadow: 1px 1px 4px rgb(99 99 99 / 15%);
+  display: block;
+}
+.el-aside {
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 60px;
+  bottom: 0;
+}
+.el-main {
+  position: absolute;
+  left: 300px;
+  right: 0;
+  top: 60px;
+  bottom: 0;
+  overflow-y: scroll;
+}
+.el-menu{
+  border-right: solid 0;
+}
 .home-container {
   height: 100%;
 }
 
 .el-header {
-  background-image: url(../images/background.png);
+  /*height: 70px;*/
+  background-color: #0b2342;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
 #title {
+  padding: 0 30px;
   color: aliceblue;
-  font-size: 45px;
-  position: absolute;
-  top: 14px;
-  left: 40px;
+  font-size: 16px;
+  font-weight: 400;
+  font-family: Chinese Quote, -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
 }
 
 .el-aside {
-  background-color: #545c64;
+  background-color: #fafafa;
 }
+
+/*.el-aside {*/
+/*  background-color: #545c64;*/
+/*}*/
 
 .el-main {
   background-color: white;
@@ -291,7 +366,7 @@ export default {
   position: relative;
   width: 50px;
   height: 50px;
-  margin-right: 45px;
+  margin-right: 10px;
 }
 
 .userMessage {
@@ -324,4 +399,7 @@ p {
   cursor: pointer;
 }
 
+.head_img {
+  padding: 5px 10px 0;
+}
 </style>
