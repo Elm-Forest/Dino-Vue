83a79b99c83cd9294f4281a0339dc7ecb36fdfef
Elm Forest <timbrightland@gmail.com>
2023-02-07 00:29:40 +0800

    修复注册表单验证bug

--- src/components/register.vue
+++ src/components/register.vue
@@ -8,29 +8,34 @@
         <h3>已有账号？</h3>
         <h5>立即登录账号，开始便捷办公之旅</h5>
       </div>
-      <el-button round id="myButton" @click="gotolo">去登录</el-button>
+      <el-button round id="myButton" @click="login">去登录</el-button>
     </div>
     <!-- 注册框区域 -->
     <div id="login">
       <el-card class="box-card">
         <h1 id="login-title">注册</h1>
-        <el-form id="form" ref="registerFormRef" :model="registerForm" :rules="registerFormRules">
+        <el-form id="form" ref="registerFormRef" :model="registerForm" :rules="rules">
           <el-form-item prop="username">
             <el-input prefix-icon="el-icon-user-solid" v-model="registerForm.username" placeholder="请输入用户名"
-                      clearable></el-input>
+                      name="username" clearable></el-input>
           </el-form-item>
           <el-form-item prop="email">
             <el-input prefix-icon="el-icon-message" v-model="registerForm.email" placeholder="请输入邮箱"
                       clearable></el-input>
           </el-form-item>
-          <el-form-item prop="yzcode">
+          <el-form-item prop="code">
             <el-col :span="14" style="margin-right: 12px">
               <el-input
-                  prefix-icon="el-icon-edit" v-model="registerForm.yzcode" placeholder="请输入验证码"
+                  prefix-icon="el-icon-edit" v-model="registerForm.code" placeholder="请输入验证码"
                   clearable></el-input>
             </el-col>
             <el-col :span="6">
-              <el-button @click="send">发送验证码</el-button>
+              <el-button
+                  type="primary"
+                  @click="send"
+                  :disabled="disable">
+                {{ buttonName }}
+              </el-button>
             </el-col>
           </el-form-item>
           <el-form-item prop="password">
@@ -49,10 +54,9 @@
       <div id="date">{{ date }}</div>
     </div>
     <!-- 版权区域 -->
-    <div id="copyright">copyright © 张晋铭 赵佳琪 李嘉超 李子涵 张良怡 李张弛</div>
+    <div id="copyright">Copyright © 2023 AutoOffice</div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Register',
@@ -62,7 +66,7 @@ export default {
       registerForm: {
         username: '',
         email: '',
-        yzcode: '',
+        code: '',
         password: ''
       },
       // 注册表单验证规则
@@ -78,15 +82,15 @@ export default {
             message: '邮箱格式不正确',
             trigger: 'blur',
             transform(value) {
-              if (!/^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/.test(value)) {
+              if (!/^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+(([.\-])[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/.test(value)) {
                 return true
               } else {
               }
             }
           },
-          {type: 'string', message: '长度应在 12 到 17 个字符', trigger: 'blur', ming: 12, max: 17}
+          {min: 5, max: 31, type: 'string', message: '长度应在 5 到 31 个字符', trigger: 'blur'}
         ],
-        yzcode: [
+        code: [
           {required: true, message: '请输入邮箱验证码', trigger: 'blur'},
           {min: 6, max: 6, message: '长度应为 6 个字符', trigger: 'blur'}
         ],
@@ -95,41 +99,79 @@ export default {
           {min: 6, max: 15, message: '长度应在 6 到 15 个字符', trigger: 'blur'}
         ]
       },
+      sendFormRules: {},
+      rules: {
+        email: [
+          {required: true, message: '请填写邮箱', trigger: 'blur'},
+          {
+            type: 'string',
+            message: '邮箱格式不正确',
+            trigger: 'blur',
+            transform(value) {
+              if (!/^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+(([.\-])[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/.test(value)) {
+                return true
+              } else {
+              }
+            }
+          },
+          {type: 'string', message: '长度应在 5 到 17 个字符', trigger: 'blur', min: 5, max: 31}
+        ],
+      },
       time: '',
       date: '',
+      buttonName: "获取验证码",
+      count: 60,
+      disable: false,
     }
   },
   // 时间更新
   mounted() {
+    this.sendFormRules = this.rules
     this.$nextTick(() => {
       setInterval(this.update_clock, 1000);
     })
   },
   methods: {
+    block() {
+      const timeout = setInterval(() => {
+        if (this.count < 1) {
+          this.disable = false;
+          this.buttonName = "获取验证码";
+          this.count = 60;
+          clearInterval(timeout);
+        } else {
+          this.disable = true;
+          this.buttonName = this.count-- + "s后重发";
+        }
+      }, 1000);
+    },
     send() {
-      if (this.registerForm.email !== '' && this.registerForm.email !== undefined) {
-        this.$axios({
-          method: 'post',
-          url: '/email/register',
-          params: {
-            'email': this.registerForm.email,
-          }
-        }).then(function (response) {
-          console.log(JSON.stringify(response.data));
-        }).catch(function (error) {
-          console.log(localStorage.getItem('token'))
-          console.log(error);
-        });
-      } else {
-        this.$message({
-          message: "邮箱不能为空",
-          type: 'warning'
-        });
-      }
-
+      this.rules = this.sendFormRules;
+      let this_vue = this;
+      this.$refs.registerFormRef.validate(valid => {
+        if (valid) {
+          console.log(valid)
+          this.$axios({
+            method: 'post',
+            url: '/email/register',
+            params: {
+              'email': this.registerForm.email,
+            }
+          }).then(() => {
+            this_vue.$message({
+              message: "已发送验证码",
+              type: 'success'
+            })
+          }).catch(function (error) {
+            console.log(error);
+          });
+          this.block();
+        }
+      })
     },
     register() {
       let this_vue = this;
+      this.rules = this.registerFormRules;
       this.$refs.registerFormRef.validate(valid => {
         if (valid) {
           this.$axios({
@@ -139,7 +181,7 @@ export default {
               'username': this.registerForm.username,
               'password': this.registerForm.password,
               'email': this.registerForm.email,
-              'code': this.registerForm.yzcode,
+              'code': this.registerForm.code,
             }
           }).then(function (response) {
             if (response.flag) {
@@ -154,9 +196,7 @@ export default {
                 type: 'warning'
               });
             }
-            console.log(JSON.stringify(response.data));
           }).catch(function (error) {
-            console.log(localStorage.getItem('token'))
             console.log(error);
           });
         } else {
@@ -167,11 +207,9 @@ export default {
         }
       });
     },
-    // 跳转到登录页面
-    gotolo() {
+    login() {
       this.$router.push({path: '/login'})
     },
-    // 时间设置
     update_clock: function () {
       let d = new Date();
       let year = d.getFullYear();
@@ -220,7 +258,7 @@ export default {
   background: rgb(230, 162, 173);
   border-radius: 0 0 0 600px;
   position: absolute;
-  right: 0%;
+  right: 0;
 }
 
 #goto {
@@ -246,8 +284,8 @@ export default {
 
 #login-title {
   text-align: center;
-  margin-bottom: 0px;
-  margin-top: 0px;
+  margin-bottom: 0;
+  margin-top: 0;
 }
 
 .el-card {
@@ -270,7 +308,7 @@ export default {
   left: 0;
 }
 
-.myYzcode .el-form-item__error {
+.code .el-form-item__error {
   position: absolute;
   left: 20px;
 }
@@ -295,7 +333,7 @@ export default {
   color: aliceblue;
   position: absolute;
   top: 95%;
-  left: 35%;
+  left: 45%;
 }
 
 </style>
