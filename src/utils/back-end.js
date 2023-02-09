dc4e1e9d8c07893ae65df316d0d7b0ac7dff5af7
Elm Forest <timbrightland@gmail.com>
2023-02-09 21:29:54 +0800

    自动发布

--- src/utils/back-end.js
+++ src/utils/back-end.js
@@ -1,3 +1,4 @@
 const localUrl = '127.0.0.1:8080'
-const remoteUrl = '101.132.249.251:6633'
-export const requestUrl = remoteUrl
+const remoteUrl1 = '101.132.249.251:6633'
+const remoteUrl2 = '123.249.73.113:8080'
+export const requestUrl = remoteUrl2
